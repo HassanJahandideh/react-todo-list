9d6f04f7af33bdae1f82f52b5fa6bd6712f35da5
Hassan Jahandideh <jahandideh.2083@gmail.com>
2025-01-13 15:40:43 +0300

    making deleteTask functional

--- todo-list/src/TodoList.jsx
+++ todo-list/src/TodoList.jsx
@@ -16,7 +16,8 @@ function TodoList() {
    }
 
    function deleteTask(index) {
-
+      const deletingTask = tasks.filter((_, i) => i !== index);
+      setTasks(deletingTask);
    }
 
    function moveTaskUp(index) {
